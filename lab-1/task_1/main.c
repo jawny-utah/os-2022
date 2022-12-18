326381084dcb8e18f392c10ee3c40a363e8dfc27
jawny-utah <dsheva@daisybill.com>
2022-12-18 23:43:14 +0200

    Initial commit

---
+++ lab-1/task_1/main.c
@@ -0,0 +1,30 @@
+#include "stdio.h"
+#include "unistd.h"
+
+int main() {
+    int waitTime = 20;
+
+    if (fork() == 0) {
+        if (fork() == 0) {
+          sleep(waitTime);
+          return 0;
+        }
+        if (fork() == 0) {
+          sleep(waitTime);
+          return 0;
+        }
+        if (fork() == 0) {
+          sleep(waitTime);
+          return 0;
+        }
+    }
+    if (fork() == 0) {
+        if (fork() == 0) {
+          sleep(waitTime);
+          return 0;
+        }
+    }
+
+    sleep(waitTime);
+    return 0;
+}
