d7f1af50cc263b58e1c0ca3ee5c67e438d99bac5
jawny-utah <dsheva@daisybill.com>
2022-12-25 21:49:40 +0200

    Laba 2

---
+++ lab-2/src/main.rs
@@ -0,0 +1,25 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+mod vga_buf;
+
+use core::fmt::Write;
+use core::panic::PanicInfo;
+use crate::vga_buf::{Alignment, Color, Screen};
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+  loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+
+  let mut screen = Screen::new(Color::LIGHT_GREEN as u8, Alignment::Center);
+
+  for i in 0..100 {
+    write!(screen, "Number {}\n", i);
+  }
+
+  loop {}
+}
